6e313e7b679dfa42ad5b05e76184ef41d4b99f31
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-05-15 19:55:49 +0200

    fixed connection issue, creates: alarm, article and store tables, added documentation and initial preparedstatement check

--- src/main/java/nl/utwente/m4/lossprevention/database/createDB.java
+++ src/main/java/nl/utwente/m4/lossprevention/database/createDB.java
@@ -1,49 +1,75 @@
 package nl.utwente.m4.lossprevention.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class createDB {
 
     public static void driverLoader() {
         try {
             Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException cnfe) {
-            System.err.println("Error loading driver: " + cnfe);
+        } catch (ClassNotFoundException cnfE) {
+            System.err.println("Error loading driver: " + cnfE);
         }
 
     }
 
     public static void getConnection() {
         try {
+            // Sets basis for connection
             String host = "bronto.ewi.utwente.nl";
-            String dbName = "dab_di20212b";
+            String dbName = "dab_di20212b_225";
             String url = "jdbc:postgresql://"
-                    + host + ":5432/" + dbName + "?currentSchema=Nedap";
+                    + host + ":5432/" + dbName + "?currentSchema=nedap";
 
+            // Sets credentials
             String username = "dab_di20212b_225";
             String password = "4gPNr326lyRQcR1J";
             Connection connection =
                     DriverManager.getConnection(url, username, password);
 
-            String query = "SELECT DISTINCT dir.name " +
-                    " FROM movies.movie mv, movies.acts ac, movies.person p, movies.writes d, movies.person dir " +
-                    " WHERE (mv.mid = ac.mid AND p.pid = ac.pid) AND (p.name LIKE ?) AND (mv.mid = d.mid AND d.pid=dir.pid)";
+            // SQL query
+            String query = "CREATE TABLE nedap.Store (id INT PRIMARY KEY CHECK (id >= 0),latitude INT,longitude INT); CREATE TABLE nedap.Alarm (\n" +
+                    "    epc INT PRIMARY KEY,\n" +
+                    "    timestamp TIMESTAMP,\n" +
+                    "    store_id INT,\n" +
+                    "    CONSTRAINT fk_store_id\n" +
+                    "         FOREIGN KEY(store_id)\n" +
+                    "             REFERENCES nedap.Store(id),\n" +
+                    "    article_id INT,\n" +
+                    "    CONSTRAINT fk_article_id\n" +
+                    "         FOREIGN KEY(article_id)\n" +
+                    "             REFERENCES nedap.Article(id)\n" +
+                    ");CREATE TABLE nedap.Article (\n" +
+                    "    id BIGINT PRIMARY KEY CHECK (id > 0),\n" +
+                    "    category INT CHECK (category > 0),\n" +
+                    "    product INT CHECK (product > 0),\n" +
+                    "    color VARCHAR(80),\n" +
+                    "    size VARCHAR(80),\n" +
+                    "    price DECIMAL(7, 2) CHECK (price >= 0)\n" +
+                    ");";
 
             PreparedStatement st =
                     connection.prepareStatement(query);
 
 
-            st.executeQuery();
+            // Check if query needs input for prepared statement.
+            if (query.contains("?")) {
+                //st.setString();
+            }
 
+            ResultSet resultSet =
+                    st.executeQuery();
+
+            // prints query results
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
 
             connection.close();
 
 
-        } catch (SQLException sqle) {
-            System.err.println("Error connecting: " + sqle);
+        } catch (SQLException sqlE) {
+            System.err.println("Error connecting: " + sqlE);
         }
     }
 
