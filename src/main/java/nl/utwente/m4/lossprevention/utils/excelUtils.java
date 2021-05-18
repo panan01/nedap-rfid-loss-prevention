b1291e289071675e7d99b576d7640efcbc482f78
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-05-18 22:31:26 +0200

    read cell content (as a String)

--- src/main/java/nl/utwente/m4/lossprevention/utils/excelUtils.java
+++ src/main/java/nl/utwente/m4/lossprevention/utils/excelUtils.java
@@ -1,5 +1,6 @@
 package nl.utwente.m4.lossprevention.utils;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
@@ -12,6 +13,8 @@ public class excelUtils {
 
     public static void main(String[] args) {
         System.out.println(getRowCount(read("20210503_UTwente_Nedap_Stores.xlsx")));
+        System.out.println(getCellData(read("20210503_UTwente_Nedap_Stores.xlsx"),0,0));
+        System.out.println(getCellData(read("20210503_UTwente_Nedap_Stores.xlsx"),1,1));
     }
 
     public static XSSFSheet read(String sheetName) {
@@ -40,9 +43,26 @@ public class excelUtils {
         }
     }
 
+    /**
+     * Function for retrieving the amount of rows (including labels) in the .xlsx file
+     * @param sheet
+     * @return amount of rows as an int
+     */
     public static int getRowCount(XSSFSheet sheet) {
-        // Read rowcount
         return sheet.getPhysicalNumberOfRows();
+    }
+
+    /**
+     * Function for retrieving the content of a specific cell in the .xlsx file given the row and column indexes
+     * @param sheet
+     * @param row
+     * @param column
+     * @return cell contents as a String
+     */
+    public static String getCellData(XSSFSheet sheet, int row, int column){
+        // As just retrieving the cell content trough a getNumeric/getString value requires multiple functions and checks a dataFormatter is the optimal solution as it returns a string from any datainput from the cell.
+        DataFormatter dataFormatter = new DataFormatter();
+        return dataFormatter.formatCellValue(sheet.getRow(row).getCell(column));
     }
 
 }
