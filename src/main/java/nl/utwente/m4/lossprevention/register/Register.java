0353cbbfac4dd7cb64285caeb8436cc5bfc33856
An <a.pan@student.utwente.nl>
2021-06-02 11:29:55 +0200

    Updated user with Salt and Storing hashPass as Base64

--- src/main/java/nl/utwente/m4/lossprevention/register/Register.java
+++ src/main/java/nl/utwente/m4/lossprevention/register/Register.java
@@ -10,12 +10,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.security.SecureRandom;
 
 
 @Path("/register")
 public class Register {
     private final String salt = "$1$N8qsKOcF$dWj1idimpoRJbyVJhU4uk1";
-
+    private final String gen = "!\\\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    private SecureRandom rnd = new SecureRandom();
     /*
         JSON FILE SEND FROM FRONT END FOR REGISTER USER
         {
@@ -35,12 +37,21 @@ public class Register {
         String first_name = (String) body.get("first_name");
         String last_name = (String) body.get("last_name");
         String type = (String) body.get("type");
-        byte[] hashed_pass = PasswordHasher.instance.hashPassword(password);
+        String salt = this.saltGenerator();
+        byte[] hashed_pass = PasswordHasher.instance.hashPassword(password, salt);
         if (Queries.instance.checkEmailValidity(email) && hashed_pass != null){
-            boolean queryResult = Queries.instance.addNewUser(email, hashed_pass, first_name, last_name, type);
+            boolean queryResult = Queries.instance.addNewUser(email, hashed_pass, first_name, last_name, type, salt);
             return queryResult ? Response.status(200).entity("success").build() : Response.status(200).entity("fail").build();
         }
         return Response.status(200).entity("fail").build();
+    }
+
+    public String saltGenerator(){
+        String result = "";
+        for (int i = 0; i < 16; i++) {
+            result += gen.charAt(rnd.nextInt(gen.length()));
+        }
+        return result;
     }
 
 }
