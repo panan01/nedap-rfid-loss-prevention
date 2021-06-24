210c86e1b08c1b9fbbf87fea88ccdd355c5c3c6e
Mart Postma <m.postma@student.utwente.nl>
2021-06-24 21:16:14 +0200

    back-end security changes for file upload

--- src/main/java/nl/utwente/m4/lossprevention/utils/FileUploadServlet.java
+++ src/main/java/nl/utwente/m4/lossprevention/utils/FileUploadServlet.java
@@ -1,9 +1,12 @@
 package nl.utwente.m4.lossprevention.utils;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.poifs.filesystem.FileMagic;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLConnection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,19 +23,25 @@ public class FileUploadServlet extends HttpServlet {
         // get the InputStream
         InputStream inputStream = filePart.getInputStream();
 
-        XSSFSheet sheet = excelUtils.read(inputStream);
         try {
-            if (sheet == null) {
-                response.getWriter().println("Error: Could not read excel file.");
-            } else {
-                String result = sqlUtils.XSSFSheet_to_DB(sheet);
-                response.getWriter().println("File upload was a "
-                        + (result.equals("Status-0") ? "success." : ("failure! status=" + result)));
+            if (FileMagic.valueOf(inputStream).equals(FileMagic.BIFF2) || FileMagic.valueOf(inputStream).equals(FileMagic.BIFF3) || FileMagic.valueOf(inputStream).equals(FileMagic.BIFF4)) {
+                XSSFSheet sheet = excelUtils.read(inputStream);
+                try {
+                    if (sheet == null) {
+                        response.getWriter().println("Error: Could not read excel file.");
+                    } else {
+                        String result = sqlUtils.XSSFSheet_to_DB(sheet);
+                        response.getWriter().println("File upload was a "
+                                + (result.equals("Status-0") ? "success." : ("failure! status=" + result)));
+                    }
+                } catch (Exception e) {
+                    response.getWriter().println("Error: Could not connect to database!\n");
+                    e.printStackTrace(response.getWriter());
+                    response.getWriter().println("connection = " + sqlUtils.getConnection());
+                }
             }
-        } catch (Exception e) {
-            response.getWriter().println("Error: Could not connect to database!\n");
-            e.printStackTrace(response.getWriter());
-            response.getWriter().println("connection = " + sqlUtils.getConnection());
+        } catch (IOException e) {
+            response.getWriter().println("Error: Could not determine file type");
         }
     }
 }
