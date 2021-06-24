8458eef1cef1f7f6efc554ac1c75e9f4793e2ffd
Mart Postma <m.postma@student.utwente.nl>
2021-06-24 21:55:09 +0200

    additional security checks for file upload in the back-end

--- src/main/java/nl/utwente/m4/lossprevention/utils/FileUploadServlet.java
+++ src/main/java/nl/utwente/m4/lossprevention/utils/FileUploadServlet.java
@@ -1,12 +1,12 @@
 package nl.utwente.m4.lossprevention.utils;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.poi.poifs.filesystem.FileMagic;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLConnection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,8 +23,16 @@ public class FileUploadServlet extends HttpServlet {
         // get the InputStream
         InputStream inputStream = filePart.getInputStream();
 
+        // create Workbook and Sheet instances to validate user's file
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet uploadedSheet = workbook.getSheetAt(0);
+
+        // check whether the file has at least 10 rows AND exactly 6 OR minimum 3 rows (standard NEDAP format)
+        boolean correctFormat = uploadedSheet.getLastRowNum() > 10 && uploadedSheet.getRow(1).getLastCellNum() == 6 && uploadedSheet.getRow(1).getLastCellNum() == 3;
+
+        // check once more in back-end whether the file type is .xlsx (=BIFF2/3/4)
         try {
-            if (FileMagic.valueOf(inputStream).equals(FileMagic.BIFF2) || FileMagic.valueOf(inputStream).equals(FileMagic.BIFF3) || FileMagic.valueOf(inputStream).equals(FileMagic.BIFF4)) {
+            if (correctFormat && (FileMagic.valueOf(inputStream).equals(FileMagic.BIFF2) || FileMagic.valueOf(inputStream).equals(FileMagic.BIFF3) || FileMagic.valueOf(inputStream).equals(FileMagic.BIFF4))) {
                 XSSFSheet sheet = excelUtils.read(inputStream);
                 try {
                     if (sheet == null) {
